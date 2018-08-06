66b37f6c3fc37943b198ffb3d636e92a79949754
ruwandi <ruwandi1102@gmail.com>
2018-08-06 09:05:10 +0530

    !--admin page--!

---
+++ admin.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+      <title></title>
+        <link href="//db.onlinewebfonts.com/c/a4e256ed67403c6ad5d43937ed48a77b?family=Core+Sans+N+W01+35+Light" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="home.css" type="text/css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body style="color:white;">
+      <div class="container">
+        <nav class="navbar navbar-inverse">
+          <div class="container-fluid">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+              <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+          <center>
+            <h1>Sabaragamuwa University of Sri Lanka</h1>
+            <h2>Application Form</h2>
+            <h3>UNIVERSITY ADMISSION- ACADEMIC YEAR 2017/2018</h3>
+          </center>
+
+           
+  </div>
+</body>
+</html>
